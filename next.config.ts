1e985854b4adb7a45a609c009534d8d7459b65c2
fedotarte <artyom.fedotov93@gmail.com>
2026-01-12 08:59:57 +0300

    chore: check basepath

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
-  basePath: process.env.NODE_ENV === "production" ? "/glau" : "/",
   output: "standalone",
 };
 
